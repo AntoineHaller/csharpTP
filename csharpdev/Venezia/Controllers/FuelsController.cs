8ce59b57315d5a3d6cacae93d14fc46918ac6e87
AntoineHaller <antoine.haller@ynov.com>
2020-10-22 17:48:34 +0200

    commit

---
+++ csharpdev/Venezia/Controllers/FuelsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Venezia.Data;
+using Venezia.Models;
+
+namespace Venezia.Controllers
+{
+    public class FuelsController : Controller
+    {
+        private readonly VeneziaContext _context;
+
+        public FuelsController(VeneziaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Fuels
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Fuel.ToListAsync());
+        }
+
+        // GET: Fuels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fuel = await _context.Fuel
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (fuel == null)
+            {
+                return NotFound();
+            }
+
+            return View(fuel);
+        }
+
+        // GET: Fuels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Fuels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Label")] Fuel fuel)
+        {
+            //Vérification que l'utilisateur soit bien connecté
+            if (ModelState.IsValid && HttpContext.User.Identity.IsAuthenticated)
+            {
+                _context.Add(fuel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            //Si l'utilisateur n'est pas connecté on enregistre pas la voiture et on redirige l'utilisateur sur la page de connection
+            if (ModelState.IsValid && !HttpContext.User.Identity.IsAuthenticated) Response.Redirect("/Identity/Account/login");
+
+            return View(fuel);
+        }
+
+        // GET: Fuels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fuel = await _context.Fuel.FindAsync(id);
+            if (fuel == null)
+            {
+                return NotFound();
+            }
+            return View(fuel);
+        }
+
+        // POST: Fuels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Label")] Fuel fuel)
+        {
+            if (id != fuel.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(fuel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FuelExists(fuel.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fuel);
+        }
+
+        // GET: Fuels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fuel = await _context.Fuel
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (fuel == null)
+            {
+                return NotFound();
+            }
+
+            return View(fuel);
+        }
+
+        // POST: Fuels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var fuel = await _context.Fuel.FindAsync(id);
+            _context.Fuel.Remove(fuel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FuelExists(int id)
+        {
+            return _context.Fuel.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ csharpdev/Venezia/TagHelpers/UserTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Venezia.TagHelpers
+{
+    public class UserTagHelper : TagHelper
+    {
+        [ViewContext]
+        public ViewContext Context { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //Si un utilisateur est connecté
+            if (Context.HttpContext.User.Identity.Name != null)
+            {
+                var data = Context.HttpContext.User.Identity.Name.ToString();
+                output.Content.SetContent(data);
+            }
+        }
+    }
+}
